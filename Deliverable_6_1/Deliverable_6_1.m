4273f136f41c47663651cf88ff82b14038371a4b
brahimrejeb <brahim.rejeb@gmail.com>
2023-01-12 18:21:47 +0100

    Part 6 (still needs tuning)

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -1,16 +1,37 @@
-Ts = 1/20;
+Ts = 1/20; 
 rocket = Rocket(Ts);
-H = 3; % Horizon length in seconds
+H = 3;
+x0 = zeros(12,1);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+
+%----------First experiment: NMPC and default maximum angle (15°)-----------%
+% change Q and R cost matrices as follows : Q = diag([0 0 0 0 0 100 0 0 0 100 100 10]);R = diag([0.1 0.1 0.1 0.1]);
 nmpc = NmpcControl(rocket, H);
-% MPC reference with default maximum roll = 15 deg
-ref = @(t_ , x_ ) ref_EPFL(t_);
-% MPC reference with specified maximum roll = 50 deg
-roll_max = deg2rad(50);
-%ref = @(t_ , x_ ) ref_EPFL(t_ , roll_max);
-% Evaluate once and plot optimal open−loop trajectory,
-% pad last input to get consistent size with time and state
-[u, T_opt, X_opt, U_opt] = nmpc.get_u(x, ref);
-U_opt(:,end+1) = nan;
-ph = rocket.plotvis(T_opt, X_opt, U_opt, ref);
+ref15 = @(t_ , x_ ) ref_EPFL(t_);
 Tf = 30;
-[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref15);
+rocket.anim_rate = 3;
+ph = rocket.plotvis(T, X, U,Ref); 
+
+%----------Second experiment: NMPC and specified maximum angle (50°)-----------%
+% change Q and R cost matrices as follows : Q = diag([0 0 0 0 0 100 0 0 0 100 100 10]);R = diag([0.1 0.1 0.1 0.1]);
+roll_max = deg2rad(50);
+ref50 = @(t_ , x_ ) ref_EPFL(t_,roll_max);
+Tf = 30;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref50);
+ph = rocket.plotvis(T, X, U,Ref); 
+
+%----------Third experiment: Linear MPC and the specified maximum angle (50°)-----------%
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+% Setup reference function
+ref = @(t_ , x_ ) ref_EPFL(t_,roll_max);
+Tf = 30;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
+ph = rocket.plotvis(T, X, U, Ref);
+
